eab5bab4326723ca67eb9a820880291dbf7c9781
Greg Williams <greggwilliams58@hotmail.com>
2020-06-24 14:24:01 +0100

    Inital committ2

---
+++ ETL Of Data Warehouse Data/ETL Of Data Warehouse Data/CSV_DM.py
@@ -0,0 +1,196 @@
+import pandas as pd
+import sqlalchemy 
+import numpy as np
+from sqlalchemy import create_engine, MetaData, Table, select, inspect
+
+
+def main():
+    """
+    This code takes in a csv file representing delay minutes and converts it to the required formats
+    """
+    #import raw csv file
+    raw_data = pd.read_csv("input\Detailed Delay Minutes 2014-15 to 2019-20 All Regions.csv",encoding='cp1252')
+    
+    #drop unnecessary columns and concat three columns into one
+    raw_data = shapecolumns(raw_data)
+
+    #get the financial_period lookup info
+    dimt_fp = getfpdata('dbo','dimt_financial_period')
+
+    #lookup up last day of period and add to dataset
+    dated_data = handledates(raw_data,dimt_fp)
+
+    #stack the data into a single column for data
+    pivoted_data = stackminmaxvalues(dated_data)
+
+    #add new static columns
+    prepared_data = addnewcolumns(pivoted_data)
+
+    #export finished dataset
+    print("exporting data.  This might take a while.")
+    prepared_data.to_csv('output/formatted_DM.csv', index=False)
+
+
+def shapecolumns(df):
+    """
+    This procedure drops unnecessary columns and creates option_1 column by concatting three columns together
+
+    Parameter:
+    df          A dataframe holding the raw data
+
+    Returns:
+    df          A dataframe holding the transformed dataframe
+    """
+    print("dropping unnecessary columns\n")
+    #get rid of uncessary columns
+    df = df.drop(['Year','Route','Route.Name Original','Route Name Amended','Area','Area.Name',
+                              'Delivery.Unit','Incident.Summary.Group','Incident.Category.Description',
+                              'Responsible.Organisation','Responsible.Manager','Responsible.Manager.Name',
+                              'Responsible.Function.Level.3.Desc','Responsible.Function.Level.3.Name'],axis=1)
+
+    #join the three columns together
+    df['Option_1'] = df['Incident.Reason.Description'] + "[" + df['Incident.Category'] + "]" + "[" + df['Incident.Reason'] + "]"
+
+    #drop unnecessary columns
+    df = df.drop(['Incident.Reason.Description','Incident.Category','Incident.Reason'],axis= 1 )
+
+    return df
+
+
+def handledates(raw_dataset,fp):
+    """
+    This procedure takes the financial period column and converts it to a timedate format representing the last day of the period.
+
+    Parameters
+    raw_dataset:        A dataframe holding the data
+    fp:                 A dataframe holding the financial_period_key and last_day_of_period information
+
+    Returns
+    joined_data:        A dataframe holding the date information
+    """
+    print("adding date information in required format\n")
+    #format the date into fp key format as an int
+    raw_dataset['Financial.Year...Period'] = raw_dataset['Financial.Year...Period'].str.replace('/','20')
+    raw_dataset['Financial.Year...Period'] = raw_dataset['Financial.Year...Period'].str.replace('_P','')
+    raw_dataset['Financial.Year...Period'] = raw_dataset['Financial.Year...Period'].astype('int32')
+
+    joined_data = pd.merge(raw_dataset,fp,how='inner',left_on='Financial.Year...Period',
+                           right_on='financial_period_key')
+
+    joined_data['Financial.Year...Period'] = joined_data['financial_period_end_date']
+    del joined_data['financial_period_end_date']
+    
+    return joined_data
+
+
+
+def getfpdata(schema_name,table_name):
+    """
+    This uses SQL Alchemy to connect to SQL Server via a trusted connection and extract a filtered table, which is then coverted into a dataframe.
+    This is intended for getting financial_period_data
+
+    Parameters
+    schema_name:    A string represetnting the schema of the table
+    table_name:     A string representing the name of the table
+
+
+    returns:        
+    df:             A dataframe containing columns financial_period_key and financial_period_end_date 
+    """
+
+    print(f"getting DW data from {table_name}\n")
+
+    engine = sqlalchemy.create_engine('mssql+pyodbc://AZORRDWSC01/ORR_DW?driver=SQL+Server+Native+Client+11.0?trusted_connection=yes')
+    
+    conn = engine.connect()
+
+    metadata = MetaData()
+
+    example_table = Table(table_name, metadata,autoload=True, autoload_with=engine, schema=schema_name)
+
+    #get raw table data, filtered by source_item_id
+    query = select([example_table.c.financial_period_key,example_table.c.financial_period_end_date])
+    
+    df = pd.read_sql(query, conn)
+    
+    return df
+
+
+
+def stackminmaxvalues(unpivoted_data):
+    """
+    This takes the DM data and stacks the data into a single column.  It also calculates merges the min/max values for DM and Count by 
+    converting the irrelevant column into 0 and then adding that to the relevant column.
+
+    Parameter
+    unpivoted_data:     A dataframe holding the data in wide format
+
+    Returns
+    new_data:           A dataframe holding the data in long format, with combined min/max values
+    """
+    print("calculating the min and max values\n")
+    
+    #calculate min and max for both minutes and incident count
+    unpivoted_data['max_inc_count'] = unpivoted_data.groupby(['Delivery.Unit.Name','Responsible.Organisation.Name','Option_1'])['v_Incident.Count'].transform(np.max)
+    unpivoted_data['min_inc_count'] = unpivoted_data.groupby(['Delivery.Unit.Name','Responsible.Organisation.Name','Option_1'])['v_Incident.Count'].transform(np.min)
+
+    unpivoted_data['max_minutes'] = unpivoted_data.groupby(['Delivery.Unit.Name','Responsible.Organisation.Name','Option_1'])['v_PfPI.Minutes'].transform(np.max)
+    unpivoted_data['min_minutes'] = unpivoted_data.groupby(['Delivery.Unit.Name','Responsible.Organisation.Name','Option_1'])['v_PfPI.Minutes'].transform(np.min)
+
+    print("reshaping the data\n ")
+    #this takes the data and stacks it with a single values column
+    new_data = pd.melt(unpivoted_data,id_vars=['Financial.Year...Period','Delivery.Unit.Name','Responsible.Organisation.Name','Option_1','min_inc_count','max_inc_count','min_minutes','max_minutes'],value_vars=['v_Incident.Count','v_PfPI.Minutes'] )
+    
+    print("reshaping min and max values\n")
+    #filter the data by variable column to get distinct list of min/max for counts/minutes
+    new_data.loc[new_data.variable == 'v_Incident.Count', 'min_minutes'] = 0
+    new_data.loc[new_data.variable == 'v_Incident.Count', 'max_minutes'] = 0
+
+    new_data.loc[new_data.variable == 'v_PfPI.Minutes', 'min_inc_count'] = 0
+    new_data.loc[new_data.variable == 'v_PfPI.Minutes', 'min_inc_count'] = 0
+    
+    #adding the columns together into a new combined column
+    new_data['min_value'] = new_data['min_minutes'] + new_data['min_inc_count']
+    new_data['max_value'] = new_data['max_minutes'] + new_data['max_inc_count']
+
+    #dropping the irrelvant columns
+    del new_data['min_inc_count']
+    del new_data['max_inc_count']
+    del new_data['min_minutes']
+    del new_data['max_minutes']
+
+    return new_data
+
+
+def addnewcolumns(dataset):
+    """
+    This renames the columns into their final name, adds the static columns and places them into the appropriate order
+    """
+    print("adding the new static columns\n")
+
+    dataset.rename(columns={'Financial.Year...Period':'Date','variable':'Option_2','Responsible.Organisation.Name':'Option_3','value':'Value'}
+                   ,inplace=True)
+    dataset['Base_Location'] = dataset['Delivery.Unit.Name']
+    dataset['TOC'] = 'x'
+    dataset['Criticality'] = 'x'
+    dataset['Location'] = dataset['Delivery.Unit.Name']
+    dataset['Location_Type'] = 'MDU'
+    dataset['Natural_Frequency'] = 'Period'
+    dataset['Datatype'] = 'Delay_Minutes'
+    dataset['Option_4'] = 'x'
+    dataset['Option_5'] = 'x'
+
+    dataset = dataset[['Base_Location', 'TOC','Criticality' ,'Location' ,'Location_Type','Natural_Frequency' ,'Datatype','Option_1','Option_2','Option_3',
+                       'Option_4','Option_5','min_value','max_value','Date','Value']]
+    
+    #copy made of dataset to avoid the "setting on copy warning"
+    final_data = dataset.copy()
+
+    final_data['Option_2'].replace(['v_Incident.Count','v_PfPI.Minutes'],['Incident_Count','Delay_Minutes'],inplace=True)
+    
+
+    return final_data
+
+
+if __name__ == '__main__':
+    main()
---
+++ ETL Of Data Warehouse Data/ETL Of Data Warehouse Data/weather.py
@@ -0,0 +1,167 @@
+from urllib.request import Request,urlopen
+import pandas as pd
+from io import StringIO
+import xlsxwriter
+import calendar
+import numpy as np
+import datetime
+
+
+def main():
+    
+    urls = get_list_of_urls()
+
+    full_dataset = list()
+
+    for url in urls:
+
+        targeturl = url
+        listofurl = targeturl.split('/')
+        data = get_weather_data(targeturl,listofurl[9],listofurl[11].replace('.txt',''))
+        full_dataset.append(data)
+
+    all_data = pd.concat(full_dataset)
+
+    all_data.to_csv('output/formatted_weather.csv', index=False)
+
+
+def get_list_of_urls():
+    urls = pd.read_excel('input/Weather data source (MetOffice).xlsx')
+    list_of_urls = urls['DATA SOURCE']
+
+    return list_of_urls
+
+
+def get_weather_data(webaddress, weather_type, region):
+    """
+    This takes a web address from the met office and extracts a .txt file which is converted into a string,
+    removing the first five introductory lines then convert the file into a pandas dataframe. It then drops
+    the unnecessary seasonal aggregations.  The data is then stacked by year and month using melt.
+
+    Parameters
+    webaddress:     A string holding the url for the txt file
+    weathertype:    A string holding the type of data held in dataset
+    region:         A string holding the region under discussion
+
+    
+    """
+    req = Request(webaddress,headers={'User-Agent': 'Mozilla/5.0'})
+    webpage = urlopen(req).read()
+    txt_string = webpage.decode('utf-8')
+        #remove unnecessary five lines of introduction
+    postString = txt_string.split("\n",5)[5]
+
+    parse_column = ['year']
+    weather_data = pd.read_csv(StringIO(postString),delimiter=r'\s{1-4}',parse_dates=parse_column, engine='python')
+
+    print(weather_data.info())
+
+    # lose unnecessary columns
+    weather_data.drop(['win','spr','sum','aut','ann'],axis=1,inplace=True)
+    
+    print(weather_data)
+    # add region and weather type information here
+    weather_data['Location'] = region
+    weather_data['Option_1'] = weather_type
+    
+    print("before min and max")
+    print(weather_data)
+
+    weather_data = pd.melt(weather_data, id_vars=['year','Location','Option_1'],value_vars=['jan','feb','mar','apr','may','jun','jul','aug','sep','oct',	'nov','dec'])
+    
+    print("this is melted data")
+    print(weather_data)
+    print(weather_data.info())
+    
+    #get min and max values
+    weather_data['max'] = weather_data.max(axis=0)['value']
+    weather_data['min'] = weather_data.min(axis=0)['value']
+
+    print("weather_data")
+
+    weather_data = handle_dates(weather_data)
+
+    formatted_data = format_full_dataset(weather_data,weather_type,region)
+
+    return formatted_data
+
+
+def handle_dates(weather):
+    weather['year'] = weather['year'].dt.year
+
+    #filter out years before 1900 that can't be parsed
+    weather = weather[weather['year']>=1900]
+
+    #convert month to int 
+    month_lookup = {'jan':int('01'),'feb':int('02'),'mar':int('03'),'apr':int('04'),'may':int('05'),'jun':int('06'),'jul':int('07')
+                    ,'aug':int('08'),'sep':int('09'),'oct':int('10'),'nov':int('11'),'dec':int('12')}
+    weather['month']= weather['variable'].apply(lambda x:month_lookup[x])
+    
+    #convert day to int
+    day_lookup = {int('01'):int('31'), int('02'):int('28'),int('03'):int('31'),int('04'):int('30'),int('05'):int('31'),int('06'):int('30'),
+                  int('07'):int('31'),int('08'):int('31'),int('09'):int('30'),int('10'):int('31'),int('11'):int('30'),int('12'):int('31')}
+
+    weather['day'] = weather['month'].apply(lambda x:day_lookup[x])
+
+    #handle leap year issue
+    weather['leap_year'] = weather['year'].apply(lambda x: calendar.isleap(x))
+    
+    conditions = [(weather['variable'] == 'feb') & (weather['leap_year'] == True)  ]
+
+    choices = [weather['day']+1]
+
+    weather['day'] = np.select(conditions,choices,default = weather['day'])
+
+    #concatinate and convert to date format
+    weather['date'] = weather['year'].astype(str) +'/'+ weather['month'].astype(str) + '/'+ weather['day'].astype(str)
+    #weather['date'] = datetime.datetime.strptime(weather['date'],'%Y%m%d')
+    pd.to_datetime(weather['date'],format = '%Y%m%d',errors='coerce')
+
+    #drop unnecessary columns
+    del weather['leap_year']
+    del weather['year']
+    del weather['month']
+    del weather['day']
+
+    return weather
+
+
+def format_full_dataset(raw_data,weather_type,region):
+    
+
+    #set weather_type correct
+    if weather_type == 'Tmax':
+        weather_type = 'Max Temperature'
+    elif weather_type == 'Tmin':
+        weather_type = 'Min Temperature'
+    elif weather_type == 'Rainfall':
+        weather_type = 'Rainfall'
+    
+    #assign new fields to datasets
+    raw_data['Base_Location'] = region
+    #raw_data['Location'] = region 
+    raw_data['TOC'] = 'x'
+    raw_data['Criticality'] = 'x'
+    raw_data['Location_Type'] = 'Met Office Region'
+    raw_data['Natural_Frequency'] = 'Monthly'
+    raw_data['Data_Type'] = 'Weather'
+    #raw_data['Option_1'] = weather_type
+    raw_data['Option_2'] = 'x'
+    raw_data['Option_3'] = 'x'
+    raw_data['Option_4'] = 'x'
+    raw_data['Option_5'] = 'x'
+    raw_data['Date'] = raw_data['date']
+    raw_data['min_value'] = raw_data['min']
+    raw_data['max_value'] = raw_data['max']
+    raw_data['Value'] = raw_data['value']
+
+
+    columntitles =['Base_Location','TOC','Criticality','Location','Location_Type','Natural_Frequency','Data_Type','Option_1','Option_2','Option_3','Option_4','Option_5','min_value','max_value','Date','value']
+
+    raw_data = raw_data.reindex(columns=columntitles)
+
+    
+    return raw_data
+
+if __name__ == '__main__':
+    main()
